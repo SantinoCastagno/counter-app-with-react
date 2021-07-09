9090280aca4c79b06667536884c5c5c4c9ad5d19
Santino Castagno <santinocastagno.dev@gmail.com>
2021-07-09 20:25:57 -0300

    Add more functions to use useState hook

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const handleIncrement = (e) => {
-   console.log(e);
-}
+const CounterApp = ({ value = 0 }) => {
+   //Hook init
+   const [counter, setCounter] = useState(value);
 
+   const handleIncrement = () => {
+      setCounter(counter + 1);
+   }
 
-const CounterApp = ({value}) => {
+   const handleDecrement = () => {
+      setCounter(counter - 1);
+   }
+
+   const handleReset = () => {
+      setCounter(value);
+   }
+
    return <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
-      <button onClick={ handleIncrement }> +1 </button>
+      <h2>{counter}</h2>
+      <button onClick={handleIncrement}> +1 </button>
+      <button onClick={handleReset}> Reset </button>
+      <button onClick={handleDecrement}> -1 </button>
    </>
 }
 
 CounterApp.propTypes = {
-   value : PropTypes.number,
+   value: PropTypes.number,
 }
 
 export default CounterApp;
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,6 @@ import './index.css';
 import CounterApp from './components/CounterApp';
 
 const root = document.querySelector('#root');
-ReactDOM.render(<CounterApp value={222}/>, root);
+ReactDOM.render(<CounterApp value={5}/>, root);
 
 

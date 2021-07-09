8ecb440b76119c6b83a48c26b1cd16ee1aa5a22e
Santino Castagno <santinocastagno.dev@gmail.com>
2021-07-09 18:58:04 -0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import FirstComponent from './FirstComponent';
+
+const root = document.querySelector('#root');
+ReactDOM.render(<FirstComponent />, root);
+
+
